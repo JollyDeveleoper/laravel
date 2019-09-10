19d4446af7fb2259fec240346fbb9595ddc05fd7
Jolly Joy <lollidark@mail.ru>
2019-09-10 17:08:56 +0400

    Фикс какой-то дичи

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Bot\BotController;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
@@ -25,13 +26,30 @@ class ScheduleController extends Controller
         return $data;
     }
 
-    public function index() {
-        $days = array("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота");
+    public function index($day = 'all') {
+        $days_title = array("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота");
+        $arr = [];
+        $current_title = '';
+        if ($day !== 'all') {
+            // Либо сегодня, либо завтра. Выбираем день
+            $new_day = self::getLoadedDay($day);
+            $arr['schedule'] =  $this->getData()['schedule'][$new_day];
 
+            // Для заголовока
+            $current_title = $day === 'today' ? 'Сегодня' : 'Завтра';
+        }
+        // Для сеглдня или завтра формируем расписание в отдельный массив
+        $current_day = $day === 'all' ? $this->getData()['schedule'] : $arr;
         return view($this->isMobile(\request()) ? 'schedule/schedule_mobile' : 'schedule/schedule', array(
-            'data' => $this->getData()['schedule'],
-            'days' => $days,
-            'day' => date('l')
+            'data' => $current_day,
+            'days_list' => $days_title,
+            'current_title' => $current_title
         ));
+    }
+
+    private static function getLoadedDay($day) {
+        $day_of_weekly = array('today' => date('l'), 'tomorrow' => BotController::getCurrentDateWithOffset(strtotime('tomorrow')));
+        $day = strtolower($day_of_weekly[$day]);
+        return $day;
     }
 }
