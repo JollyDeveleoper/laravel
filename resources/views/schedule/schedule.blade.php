b683b76788d3740fde66d6a25f776a38692e0c6c
Jolly Joy <lollidark@mail.ru>
2019-09-10 23:32:58 +0400

    Изменил дизайн пк версии

--- resources/views/schedule/schedule.blade.php
+++ resources/views/schedule/schedule.blade.php
@@ -3,51 +3,37 @@
 @section('content')
     <div class="container">
         <span class="title">Расписание</span>
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th></th>
-                <th scope="col">Время</th>
-                <th scope="col">Предмет</th>
-                <th scope="col">Препод</th>
-                <th scope="col">Ауд</th>
-            </tr>
 
-            @foreach($data as $item => $value)
+        @foreach($data as $item => $value)
 
-                @if(empty($value['name']))
-                    @break
-                @endif
-                @php
-                    $prevElement = isset($data[$item - 1]) ? $data[$item - 1] : false;
-                    $isState = !$prevElement || $value['day'] !== $prevElement['day'];
-                    $row = array_count_values($data[$item])[$item + 1];
-                @endphp
-                {{ $row }}
-
-                @if($isState)
-                <tr>
-                    <td rowspan="{{$row}}" class="font-weight-bold day">
-                        <span>
-
-                            {{__('app.days.'.$value['day'])}}
-                        </span>
-                    </td>
-                </tr>
-                @endif
-
-                <tr>
+            @if(empty($value['name']))
+                @break
+            @endif
+            @php
+                $prevElement = isset($data[$item - 1]) ? $data[$item - 1] : false;
+                $isState = !$prevElement || $value['day'] !== $prevElement['day'];
+                $row = array_count_values($data[$item])[$item + 1];
+                $isToday = date('w') === $value['day'];
+            @endphp
 
 
-                    <td>{{ $value['start_time'] . ' - ' . $value['end_time'] }}</td>
-                    <td>{{ $value['name'] }}</td>
-                    <td>{{ $value['teacher'] }}</td>
-                    <td><span>{{ $value['cabinet'] }}</span></td>
-                </tr>
-{{--                @break--}}
+            @if($isState)
+                <h2 class="mb-2 mt-2">
+                    {{__('app.days.'.$value['day'])}}
+                    @if($isToday)
+                        <small class="badge badge-pill badge-primary">Сегодня</small>
+                    @endif
+                </h2>
+            @endif
+                <div class="mr-2  mt-2 mb-3 rounded-lg shadow p-3   d-inline-block " style="min-width: 300px">
+                    <div class="card-title font-weight-bold">{{ $value['start_time'] . ' - ' . $value['end_time'] }}</div>
+                    <small>{{ $value['teacher'] }}</small>
+                    <p class="card-text">{{ $value['name'] }}</p>
+                    <span class="font-weight-bold">Кабинет:</span> {{ $value['cabinet'] }}
 
-            @endforeach
-            </thead>
-        </table>
+                </div>
+        @endforeach
+
+
     </div>
 @endsection
