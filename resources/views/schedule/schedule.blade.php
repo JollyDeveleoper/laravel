d1f492407647f9a6178033497368b882ff5d661e
Jolly Joy <lollidark@mail.ru>
2019-09-14 18:01:45 +0400

    Фиксы на пк версии

--- resources/views/schedule/schedule.blade.php
+++ resources/views/schedule/schedule.blade.php
@@ -4,36 +4,27 @@
     <div class="container">
         <span class="title">Расписание</span>
 
-        @foreach($data as $item => $value)
-
-            @if(empty($value['name']))
-                @break
-            @endif
+        @foreach($data as $item => $values)
             @php
-                $prevElement = isset($data[$item - 1]) ? $data[$item - 1] : false;
-                $isState = !$prevElement || $value['day'] !== $prevElement['day'];
-                $row = array_count_values($data[$item])[$item + 1];
-                $isToday = date('w') === $value['day'];
+                $isToday = date('w') === $values[0]['day'];
             @endphp
+            <h2 class="mb-2 mt-2">
+                {{__('app.days.'.$values[0]['day'])}}
+                @if($isToday)
+                    <small class="badge badge-pill badge-primary">Сегодня</small>
+                @endif
+            </h2>
+            @foreach($values as $val)
 
-
-            @if($isState)
-                <h2 class="mb-2 mt-2">
-                    {{__('app.days.'.$value['day'])}}
-                    @if($isToday)
-                        <small class="badge badge-pill badge-primary">Сегодня</small>
-                    @endif
-                </h2>
-            @endif
                 <div class="mr-2  mt-2 mb-3 rounded-lg shadow p-3   d-inline-block " style="min-width: 300px">
-                    <div class="card-title font-weight-bold">{{ $value['start_time'] . ' - ' . $value['end_time'] }}</div>
-                    <small>{{ $value['teacher'] }}</small>
-                    <p class="card-text">{{ $value['name'] }}</p>
-                    <span class="font-weight-bold">Кабинет:</span> {{ $value['cabinet'] }}
+                    <div class="card-title font-weight-bold">{{ $val['start_time'] . ' - ' . $val['end_time'] }}</div>
+                    <small>{{ $val['teacher'] }}</small>
+                    <p class="card-text">{{ $val['name'] }}</p>
+                    <span class="font-weight-bold">Кабинет:</span> {{ $val['cabinet'] }}
 
                 </div>
+            @endforeach
         @endforeach
-
 
     </div>
 @endsection
