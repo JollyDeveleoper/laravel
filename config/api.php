eb94d7d343b01e481266e248b44a25303585ad72
Jolly Joy <lollidark@mail.ru>
2019-09-20 13:52:56 +0400

    Обновил id чата для репоста

--- config/api.php
+++ config/api.php
@@ -13,7 +13,8 @@ return [
     'VK_API_TOKEN' => '1a6ad686a022092c8853cec85a23daec898213c03400efb8c21a4c657d3379d215f177b18b49120fc98e3',
     'VK_TEST_GROUP_ID' => -182518687,
     'VK_DPK_ID' => -170975055,
-    'VK_CHAT_ID' => 2000000001,
-    'VK_TEST_CHAT_ID' => 2000000002,
+    'VK_CHAT_ID' => 2000000001, // мы команда
+    'VK_TEST_CHAT_ID' => 2000000002, // тестовая
+    'VK_NEW_CHAT_ID' => 2000000003, // альбина черномор
 
 ];
