e5190ad8a9bf32114bb5b10e86e5e3c7efe727c1
Jolly Joy <lollidark@mail.ru>
2019-10-13 16:01:03 +0400

    Мелкие правки

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Models\Schedule;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use function request;
 
 class ScheduleController extends Controller
 {
-
     private $schedule;
 
     public function __construct(Schedule $schedule)
@@ -15,6 +18,12 @@ class ScheduleController extends Controller
         $this->schedule = $schedule;
     }
 
+    /**
+     * Выдаем все пары на неделю
+     *
+     * @param string $day
+     * @return Factory|View
+     */
     public function index($day = 'all')
     {
         $list = $this->schedule->getList($day); // исходные данные
@@ -30,17 +39,27 @@ class ScheduleController extends Controller
         ]);
     }
 
+    /**
+     * Чекаем мобильную версию и выдаем макет
+     *
+     * @return string
+     */
     private static function getView()
     {
-        $isMobile = strpos(strtolower(\request()->server('HTTP_USER_AGENT')), 'mobile');
+        $isMobile = strpos(strtolower(request()->server('HTTP_USER_AGENT')), 'mobile');
         return $isMobile ? 'schedule/mobile/schedule' : 'schedule/schedule';
     }
 
+    /**
+     * Обновляем пару по id
+     *
+     * @return RedirectResponse
+     */
     public function update()
     {
-        $id = \request('id', 0);
-        $item = $this->schedule->find($id);
+        $id = request('id', 0);
 
+        $item = $this->schedule->find($id);
         // Обновляем
         $item->update(request()->all());
 
@@ -48,19 +67,29 @@ class ScheduleController extends Controller
         return back();
     }
 
+    /**
+     * Создаем новую пару
+     *
+     * @return RedirectResponse
+     */
     public function create()
     {
-        $this->schedule->create(\request()->all());
+        $this->schedule->create(request()->all());
 
         session()->put('success', __('app.success_add'));
         return back();
-
     }
 
+    /**
+     * Удаляем определенную пару
+     *
+     * @return RedirectResponse
+     */
     public function delete()
     {
-        $deleteID = \request('deleteID');
+        $deleteID = request('deleteID');
         $this->schedule->destroy($deleteID);
+
         session()->put('success', __('app.success_delete'));
         return back();
     }
